a8b44052c1c919a57df6fccb220d7c13d29e00e6
Eric Pereira <ericpereira.np42@gmail.com>
2023-03-07 16:14:45 -0300

    Limpeza na classe jogador

--- jogador.dart
+++ jogador.dart
@@ -9,32 +9,32 @@ abstract class Jogador{
     String? input;
     for(int i = 0; i < 2; i++){
 
-    while (true){
-
-      try{
-        if(i == 0){
-          stdout.write("Digite o valor da linha:\n>>");
-        }else{
-          stdout.write("Digite o valor da coluna:\n>>");
-        }
-        Colorir.ConsoleVerde();
-        input = stdin.readLineSync();
-        Colorir.ConsolePadrao();
-      } catch(e) {
-        print(Colorir.amareloClaro("Erro! Posição inválida."));
-        continue;
-      }
-      
-      if (input != null) {
-        if (!['0', '1', '2'].contains(input)) {
+      while (true){
+        try{
+          if(i == 0){
+            stdout.write("Digite o valor da linha:\n>>");
+          }else{
+            stdout.write("Digite o valor da coluna:\n>>");
+          }
+          Colorir.ConsoleVerde();
+          input = stdin.readLineSync();
+          Colorir.ConsolePadrao();
+        } 
+        catch(e) {
           print(Colorir.amareloClaro("Erro! Posição inválida."));
-        } else {
-          break;
+          continue;
         }
-      } else {
-        print(Colorir.amareloClaro("Erro! Posição inválida."));
+        
+        if (input != null) {
+          if (!['0', '1', '2'].contains(input)) {
+            print(Colorir.amareloClaro("Erro! Posição inválida."));
+          } else {
+            break;
+          }
+        } else {
+          print(Colorir.amareloClaro("Erro! Posição inválida."));
+        }
       }
-    }
     jogada.add(int.parse(input));
     }
     return jogada;
